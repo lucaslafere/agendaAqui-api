0e0211166ccef14581708bc64aabf6f0d8356a5e
Lucas Lafèré <laferelucas@gmail.com>
2023-02-23 17:38:10 -0300

    teste'

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,6 @@ import dotenv from "dotenv";
 import app from "./app";
 dotenv.config();
 
+//teste numero dois
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
