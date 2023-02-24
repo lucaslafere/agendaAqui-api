6502eb76437f30dcd4464db88ff19c5251b0724b
Lucas Lafèré <laferelucas@gmail.com>
2023-02-24 17:06:43 -0300

    added admin login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,10 +1,18 @@
 import { Request, Response } from "express";
+import { ErrorCode } from "../middlewares/errorHandlerMiddleware";
 import { adminLoginSchema, adminSignUpSchema } from "../schemas/authSchema";
 import * as manipulateToken from "../utils/manipulateToken";
 
 export async function createAdmin(req: Request, res: Response) {
 	const { name, email, password } = req.body;
 	const { error } = adminSignUpSchema.validate({ name, email, password });
-    if (error) throw {type: , message: error.message};
+	if (error) throw { type: ErrorCode.UNPROCESSABLE, message: error.message };
 	return res.status(201).send("Created");
+}
+export async function loginAdmin(req: Request, res: Response) {
+	const { email, password } = req.body;
+	const { error } = adminLoginSchema.validate({ email, password });
+	if (error) throw { type: ErrorCode.UNPROCESSABLE, message: error.message };
+	const token = manipulateToken.generateToken({ email, password });
+	return res.status(200).send({ message: "Logged in", token });
 }
