57e139c0f278b77a967179df3da7ffd68d161c04
Lucas Lafèré <laferelucas@gmail.com>
2023-02-24 17:28:59 -0300

    better error handl;er

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import { ErrorCode } from "../middlewares/errorHandlerMiddleware";
 import { adminLoginSchema, adminRegisterSchema } from "../schemas/authSchema";
 import * as manipulateToken from "../utils/manipulateToken";
 
@@ -11,13 +10,13 @@ export async function registerAdmin(req: Request, res: Response) {
 		password,
 		confirmPassword,
 	});
-	if (error) throw { type: ErrorCode.UNPROCESSABLE, message: error.message };
+	if (error) throw { type: "wrong-body-format", message: error.message };
 	return res.status(201).send("Registered");
 }
 export async function loginAdmin(req: Request, res: Response) {
 	const { email, password } = req.body;
 	const { error } = adminLoginSchema.validate({ email, password });
-	if (error) throw { type: ErrorCode.UNPROCESSABLE, message: error.message };
+	if (error) throw { type: "wrong-body-format", message: error.message };
 	const token = manipulateToken.generateToken({ email, password });
 	return res.status(200).send({ message: "Logged in", token });
 }
--- src/middlewares/errorHandlerMiddleware.ts
+++ src/middlewares/errorHandlerMiddleware.ts
@@ -1,10 +1,18 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
+type ErrorCode =
+	| "not-found"
+	| "conflict"
+	| "unprocessable"
+	| "generic"
+	| "unauthorized"
+	| "wrong-body-format";
 interface iError {
 	type: ErrorCode;
 	message: string;
 }
-export enum ErrorCode {
+
+enum HttpStatusCode {
 	BAD_REQUEST = 400,
 	UNAUTHORIZED = 401,
 	NOT_FOUND = 404,
@@ -16,10 +24,29 @@ export enum ErrorCode {
 export default function errorHandlerMiddleware(
 	err: iError,
 	req: Request,
-	res: Response
+	res: Response,
+	_next: NextFunction
 ) {
 	console.log(err);
+	if (err.type === "not-found") {
+		return res.status(HttpStatusCode.NOT_FOUND).send(err.message);
+	}
+	if (err.type === "conflict") {
+		return res.status(HttpStatusCode.CONFLICT).send(err.message);
+	}
+	if (err.type === "unprocessable") {
+		return res.status(HttpStatusCode.UNPROCESSABLE).send(err.message);
+	}
+	if (err.type === "generic") {
+		return res.status(HttpStatusCode.BAD_REQUEST).send(err.message);
+	}
+	if (err.type === "unauthorized") {
+		return res.status(HttpStatusCode.UNAUTHORIZED).send(err.message);
+	}
+	if (err.type === "wrong-body-format") {
+		return res.status(HttpStatusCode.UNPROCESSABLE).send(err.message);
+	}
 	return res
-		.status(err.type || 500)
-		.send(err.message || "Internal Server Error");
+		.status(HttpStatusCode.INTERNAL_SERVER_ERROR)
+		.send("Internal Server Error");
 }
