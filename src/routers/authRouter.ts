8b3383c8062d05e7780b0c9e94631b8be7c2f416
Lucas Lafèré <laferelucas@gmail.com>
2023-02-24 17:36:22 -0300

    added comment for later development

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -3,7 +3,9 @@ import * as authController from "../controllers/authController";
 
 const authRouter = Router();
 
+// the register route should be protected by an admin or token middleware, so that only admins can register new admins
 authRouter.post("/register", authController.registerAdmin);
+
 authRouter.post("/login", authController.loginAdmin);
 
 export default authRouter;
